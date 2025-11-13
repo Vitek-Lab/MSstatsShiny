c08e2156aef6d834268e32becbf4875d187e7986
Rudhik1904 <rudhikshah50@gmail.com>
2025-11-12 19:19:02 -0600

    spell error

--- R/module-loadpage-server.R
+++ R/module-loadpage-server.R
@@ -12,7 +12,7 @@
 #' @examples
 #' NA
 #' 
-loadpageServer <- function(id, parent_session, is_web_server) {
+loadpageServer <- function(id, parent_session, is_web_servee) {
   moduleServer(id, function(input, output, session) {
     # toggle ui (DDA DIA SRM)
     observe({
