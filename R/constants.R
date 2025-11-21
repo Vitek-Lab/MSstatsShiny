6ad3d4a34b751c6a70b45e6c55dfb790ca3d04bf
tonywu1999 <anthonywu92@gmail.com>
2025-11-21 10:05:13 -0500

    refactor(statmodel): consolidate submit and clear comparison matrix button to one namespace (#132)

--- R/constants.R
+++ R/constants.R
@@ -1,25 +1,19 @@
 
 NAMESPACE_STATMODEL = list(
-  comparisons_conditional_panel = "comparisons_conditionalPanel",
+  comparisons_conditional_panel = "comparisons_conditional_panel",
   comparison_mode = "comparisons_mode",
-  comparisons_custom_pairwise_choice1 = "comparisons_custom_pairwise_choice1", # choice1
-  comparisons_custom_pairwise_choice2 = "comparisons_custom_pairwise_choice2", # choice2
-  comparisons_custom_pairwise_submit = "comparisons_custom_pairwise_submit", # submit
-  comparisons_custom_pairwise_clear = "comparisons_custom_pairwise_clear", # clear
-  comparisons_all_vs_one_choice = "comparisons_all_vs_one_choice", # choice3
-  comparisons_all_vs_one_submit = "comparisons_all_vs_one_submit", # submit1
-  comparisons_all_vs_one_clear = "comparisons_all_vs_one_clear", # clear1
-  comparisons_all_pairwise_submit = "comparisons_all_pairwise_submit", # submit2
-  comparisons_all_pairwise_clear = "comparisons_all_pairwise_clear", # clear2
-  comparisons_custom_nonpairwise_name = "comparisons_custom_nonpairwise_name", # comp_name
-  comparisons_custom_nonpairwise_weights = "comparisons_custom_nonpairwise_weights", # weights
-  comparisons_custom_nonpairwise_submit = "comparisons_custom_nonpairwise_submit", # submit3
-  comparisons_custom_nonpairwise_clear = "comparisons_custom_nonpairwise_clear" # clear3
+  comparisons_submit = "comparisons_submit",
+  comparisons_clear = "comparisons_clear",
+  comparisons_custom_pairwise_choice1 = "comparisons_custom_pairwise_choice1", 
+  comparisons_custom_pairwise_choice2 = "comparisons_custom_pairwise_choice2", 
+  comparisons_all_vs_one_choice = "comparisons_all_vs_one_choice", 
+  comparisons_custom_nonpairwise_name = "comparisons_custom_nonpairwise_name",
+  comparisons_custom_nonpairwise_weights = "comparisons_custom_nonpairwise_weights"
 )
 
 CONSTANTS_STATMODEL = list(
-  comparison_mode_all_pairwise = "comparison_mode_all_pairwise", # all_pair
-  comparison_mode_all_vs_one = "comparison_mode_all_vs_one", # all_one
-  comparison_mode_custom_pairwise = "comparison_mode_custom_pairwise", # custom
-  comparison_mode_custom_nonpairwise = "comparison_mode_custom_nonpairwise" # custom_np
+  comparison_mode_all_pairwise = "comparison_mode_all_pairwise", 
+  comparison_mode_all_vs_one = "comparison_mode_all_vs_one",
+  comparison_mode_custom_pairwise = "comparison_mode_custom_pairwise",
+  comparison_mode_custom_nonpairwise = "comparison_mode_custom_nonpairwise"
 )
