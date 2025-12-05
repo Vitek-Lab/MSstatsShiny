2c3f2a93733d78413316b7e407200f67d9206cb1
Tony Wu <wu.anthon@northeastern.edu>
2025-12-05 15:27:53 -0500

    solidify constants for visualization

--- R/constants.R
+++ R/constants.R
@@ -11,7 +11,22 @@ NAMESPACE_STATMODEL = list(
   comparisons_custom_nonpairwise_weights = "comparisons_custom_nonpairwise_weights",
   modeling_start = "modeling_start", # "calculate"
   modeling_significance_level = "modeling_significance_level", # "signif"
-  modeling_tmt_moderation = "modeling_tmt_moderation" # "moderated"
+  modeling_tmt_moderation = "modeling_tmt_moderation", # "moderated"
+  visualization_plot_options_conditional_panel = "plot_options_conditional_panel", # plot_specific_options
+  visualization_plot_type = "visualization_plot_type", # typeplot
+  visualization_logp_base = "visualization_logp_base", # logp
+  visualization_fold_change_checkbox = "visualization_fold_change_checkbox", # FC1
+  visualization_fold_change_input = "visualization_fold_change_input", # FC
+  visualization_volcano_which_comparison = "visualization_volcano_which_comparison", # WhichComp
+  visualization_volcano_display_protein_name = "visualization_volcano_display_protein_name", # pname
+  visualization_volcano_significance_cutoff = "visualization_volcano_significance_cutoff", # sig
+  visualization_comparison_plot_which_protein = "visualization_comparison_which_protein", # WhichProt
+  visualization_comparison_plot_which_comparison = "visualization_comparison_plot_which_comparison", # WhichComp1
+  visualization_heatmap_number_proteins = "visualization_heatmap_number_proteins", # nump
+  visualization_heatmap_cluster_option = "visualization_heatmap_cluster_option", # cluster
+  visualization_view_results = "visualization_view_results", # viewresults
+  visualization_download_plot_results = "visualization_download_plot_results" # plotresults
+  
 )
 
 CONSTANTS_STATMODEL = list(
@@ -19,5 +34,8 @@ CONSTANTS_STATMODEL = list(
   comparison_mode_all_vs_one = "comparison_mode_all_vs_one",
   comparison_mode_custom_pairwise = "comparison_mode_custom_pairwise",
   comparison_mode_custom_nonpairwise = "comparison_mode_custom_nonpairwise",
-  comparison_mode_response_curve = "comparison_mode_response_curve"
+  comparison_mode_response_curve = "comparison_mode_response_curve",
+  plot_type_volcano_plot = "plot_type_volcano_plot", # VolcanoPlot
+  plot_type_heatmap = "plot_type_heatmap", # Heatmap
+  plot_type_comparison_plot = "plot_type_comparison_plot" # ComparisonPlot
 )
