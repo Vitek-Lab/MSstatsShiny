82106eedcdf1f40cce9507275652d4ccbf8a6f4f
Rudhik1904 <rudhikshah50@gmail.com>
2026-01-08 21:05:42 -0600

    adding some of the tooltips

--- R/module-statmodel-server.R
+++ R/module-statmodel-server.R
@@ -677,6 +677,14 @@ statmodelServer = function(id, parent_session, loadpage_input, qc_input,
           ))),
           h2("Comparison matrix"),
           p(tags$i("This table is interactive. Click values to edit.")),
+          if (!is.null(input[[NAMESPACE_STATMODEL$comparison_mode]]) &&
+              input[[NAMESPACE_STATMODEL$comparison_mode]] %in% c(
+                CONSTANTS_STATMODEL$comparison_mode_all_pairwise,
+                CONSTANTS_STATMODEL$comparison_mode_all_vs_one,
+                CONSTANTS_STATMODEL$comparison_mode_custom_pairwise
+              )) {
+            p(tags$i(" -1 means control and 1 means treatment"))
+          },
           br(),
           textOutput(ns("message")),
           br(),
