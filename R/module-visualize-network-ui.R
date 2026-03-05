270031f04e2be74ccf924bd597cdcc3f32bf00f4
Tony Wu <wu.anthon@northeastern.edu>
2026-03-05 11:26:07 -0500

    create download boxes finalized

--- R/module-visualize-network-ui.R
+++ R/module-visualize-network-ui.R
@@ -357,7 +357,7 @@ createNetworkSettingsTab <- function(ns) {
                style = "margin: 0;"
              )
            ),
-           createCodeDownloadBox(ns),
+           createDownloadBoxes(ns),
            createNetworkVisualizationBox(ns),
            createEdgesTableBox(ns),
            createNodesTableBox(ns)
@@ -393,7 +393,7 @@ createDashboardBody <- function(ns) {
   )
 }
 
-createCodeDownloadBox <- function(ns) {
+createDownloadBoxes <- function(ns) {
   div(
     style = "text-align: right; padding: 10px; display: flex; justify-content: flex-end; gap: 10px;",
     uiOutput(ns("network.html.button")),
