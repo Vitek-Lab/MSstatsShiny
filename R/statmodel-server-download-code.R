c2a30aa87c74eedf10837c7e0b158c2a60ac13b0
Swaraj Patil <patil.swaraj@northeastern.edu>
2026-03-19 15:57:09 -0400

    feature(dose-response-curves): Add download analysis code for dose response curves (#180)

--- R/statmodel-server-download-code.R
+++ R/statmodel-server-download-code.R
@@ -1,6 +1,79 @@
 generate_analysis_code = function(qc_input, loadpage_input, comp_mat, input) {
   codes = preprocessDataCode(qc_input, loadpage_input)
-  
+
+  # Check if this is a response curve analysis
+  is_response_curve = !is.null(input[[NAMESPACE_STATMODEL$comparison_mode]]) &&
+    input[[NAMESPACE_STATMODEL$comparison_mode]] == CONSTANTS_STATMODEL$comparison_mode_response_curve
+
+  if (is_response_curve) {
+    increasing = input[[NAMESPACE_STATMODEL$modeling_response_curve_increasing_trend]]
+    transform_dose = input[[NAMESPACE_STATMODEL$modeling_response_curve_log_xaxis]]
+    ratio_response = isTRUE(input[[NAMESPACE_STATMODEL$visualization_response_curve_ratio_scale]])
+
+    codes = paste(codes, "\n# Set up dose response analysis\n", sep = "")
+    codes = paste(codes, "library(MSstatsResponse)\n", sep = "")
+
+    # Serialize the contrast matrix as a data frame
+    codes = paste(codes, "group_metadata = data.frame(\n", sep = "")
+    codes = paste(codes, "  GROUP = c(\"", paste(comp_mat$GROUP, collapse = "\",\""), "\"),\n", sep = "")
+    for (col_name in setdiff(colnames(comp_mat), "GROUP")) {
+      col_values = comp_mat[[col_name]]
+      if (is.numeric(col_values)) {
+        codes = paste(codes, "  ", col_name, " = c(", paste(col_values, collapse = ","), "),\n", sep = "")
+      } else {
+        codes = paste(codes, "  ", col_name, " = c(\"", paste(col_values, collapse = "\",\""), "\"),\n", sep = "")
+      }
+    }
+    codes = paste(codes, "  stringsAsFactors = FALSE\n)\n", sep = "")
+
+    # Merge and map columns explicitly (prepare_dose_response_fit is internal to MSstatsShiny)
+    codes = paste(codes, "\n# Merge metadata with protein-level data\n", sep = "")
+    codes = paste(codes, "protein_level_data = merge(summarized$ProteinLevelData, group_metadata, by = \"GROUP\")\n", sep = "")
+
+    codes = paste(codes, "\n# Map columns to MSstatsResponse format\n", sep = "")
+    codes = paste(codes, "protein_level_data$protein = protein_level_data$Protein\n", sep = "")
+    codes = paste(codes, "protein_level_data$response = protein_level_data$LogIntensities\n", sep = "")
+
+    # Determine the intervention column based on what's in the contrast matrix
+    if ("drug" %in% colnames(comp_mat)) {
+      codes = paste(codes, "protein_level_data$drug = protein_level_data$drug\n", sep = "")
+      codes = paste(codes, "protein_level_data$dose = protein_level_data$dose_value\n", sep = "")
+    } else {
+      intervention_cols = grep("time|temperature|treatment", colnames(comp_mat),
+                               ignore.case = TRUE, value = TRUE)
+      if (length(intervention_cols) > 0) {
+        intervention_type = sub("_.*", "", intervention_cols[1])
+        value_col = paste0(intervention_type, "_value")
+        codes = paste(codes, "protein_level_data$drug = \"", intervention_type, "\"\n", sep = "")
+        codes = paste(codes, "protein_level_data$dose = protein_level_data$", value_col, "\n", sep = "")
+      }
+    }
+
+    codes = paste(codes, "prepared_data = protein_level_data[, c(\"protein\", \"drug\", \"dose\", \"response\")]\n", sep = "")
+
+    codes = paste(codes, "\n# Fit dose response curves\n", sep = "")
+    codes = paste(codes, "response_results = doseResponseFit(\n",
+                  "  data = prepared_data,\n",
+                  "  increasing = ", increasing, ",\n",
+                  "  transform_dose = ", transform_dose, ",\n",
+                  "  ratio_response = FALSE\n)\n", sep = "")
+
+    codes = paste(codes, "\n# Visualize response curves\n", sep = "")
+    codes = paste(codes, "visualizeResponseProtein(\n",
+                  "  data = prepared_data,\n",
+                  "  protein_name = \"Enter protein name here\",\n",
+                  "  drug_name = \"Enter drug name here\",\n",
+                  "  ratio_response = ", ratio_response, ",\n",
+                  "  show_ic50 = TRUE,\n",
+                  "  add_ci = TRUE,\n",
+                  "  transform_dose = ", transform_dose, ",\n",
+                  "  n_samples = 1000,\n",
+                  "  increasing = ", increasing, "\n)\n", sep = "")
+
+    return(codes)
+  }
+
+  # Standard analysis - build contrast matrix
   codes = paste(codes, "\n# Create the contrast matrix\n", sep = "")
   codes = paste(codes, "contrast.matrix = NULL\n", sep = "")
   
